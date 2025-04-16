16b0095740bda722856f065a9ff9f96a6bb00bee
Malte Linke (parzival-space) <me@parzival.space>
2025-04-16 03:08:42 +0200

    Add PlaceholderParser and tests for placeholder injection functionality

---
+++ src/main/java/space/parzival/minecraft/velocity/smartmotd/util/PlaceholderParser.java
@@ -0,0 +1,41 @@
+package space.parzival.minecraft.velocity.smartmotd.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class PlaceholderParser {
+    /**
+     * Replaces all placeholders in the input string with the values from the placeholders map.
+     * Placeholders are defined as {{placeholder_name}}.
+     * @param input Replaceable string
+     * @param placeholders Map of placeholders with their parameterized function
+     * @param placeholderFunctionArgument The argument to be passed to the function
+     * @return The input string with all placeholders replaced
+     * @param <T> The type of the argument to be passed to the function
+     */
+    public static <T> String injectPlaceholders(String input, Map<String, Function<T, String>> placeholders, T placeholderFunctionArgument) {
+        for (Map.Entry<String, Function<T, String>> entry : placeholders.entrySet()) {
+            input = input.replace(
+                    "{{" + entry.getKey() + "}}",
+                    entry.getValue().apply(placeholderFunctionArgument)
+            );
+        }
+        return input;
+    }
+
+    /**
+     * Replaces all placeholders in the input string with the values from the placeholders map.
+     * @param input Replaceable Adventure Component
+     * @param placeholders Map of placeholders with their parameterized function
+     * @param placeholderFunctionArgument The argument to be passed to the function
+     * @return The input component with all placeholders replaced
+     */
+    public static <T> Component injectPlaceholders(Component input, Map<String, Function<T, String>> placeholders, T placeholderFunctionArgument) {
+        String intermediate = MiniMessage.miniMessage().serialize(input);
+        intermediate = injectPlaceholders(intermediate, placeholders, placeholderFunctionArgument);
+        return MiniMessage.miniMessage().deserialize(intermediate);
+    }
+}
---
+++ src/test/java/space/parzival/minecraft/velocity/smartmotd/util/PlaceholderParserTest.java
@@ -0,0 +1,34 @@
+package space.parzival.minecraft.velocity.smartmotd.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceholderParserTest {
+    private static final Map<String, Function<Integer, String>> placeholders = Map.of(
+            "X", String::valueOf
+    );
+
+    @Test
+    void injectPlaceholders_shouldReplace_String() {
+        String placeholderString = "Example {{X}}";
+        String expectedString = "Example 42";
+
+        String result = PlaceholderParser.injectPlaceholders(placeholderString, placeholders, 42);
+        assertEquals(expectedString, result, "Placeholder string should be replaced with the value");
+    }
+
+    @Test
+    void injectPlaceholders_shouldReplace_Component() {
+        Component placeholderComponent = MiniMessage.miniMessage().deserialize("<red>Example {{X}}</red>");
+        Component expectedComponent = MiniMessage.miniMessage().deserialize("<red>Example 42</red>");
+
+        Component result = PlaceholderParser.injectPlaceholders(placeholderComponent, placeholders, 42);
+        assertEquals(expectedComponent, result, "Placeholder component should be replaced with the value");
+    }
+}
