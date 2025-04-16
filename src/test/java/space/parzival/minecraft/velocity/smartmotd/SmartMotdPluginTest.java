9bbcc8d96697c84b34bb21c83531b6ce1c912874
Malte Linke (parzival-space) <me@parzival.space>
2025-04-16 03:37:26 +0200

    Refactor test imports to use specific assertions in PlaceholderParserTest and remove unused imports in SmartMotdPluginTest

--- src/test/java/space/parzival/minecraft/velocity/smartmotd/SmartMotdPluginTest.java
+++ src/test/java/space/parzival/minecraft/velocity/smartmotd/SmartMotdPluginTest.java
@@ -1,7 +1,5 @@
 package space.parzival.minecraft.velocity.smartmotd;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SmartMotdPluginTest {
 
 }
--- src/test/java/space/parzival/minecraft/velocity/smartmotd/util/PlaceholderParserTest.java
+++ src/test/java/space/parzival/minecraft/velocity/smartmotd/util/PlaceholderParserTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlaceholderParserTest {
     private static final Map<String, Function<Integer, String>> placeholders = Map.of(
