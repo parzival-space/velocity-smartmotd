2ea65c617ffff7979736609ef7e2a0a4767f90a2
Malte Linke <me@parzival.space>
2026-08-20 08:14:26 +0200

    Update velocity-api

--- src/main/java/space/parzival/minecraft/velocity/smartmotd/config/model/PingInformation.java
+++ src/main/java/space/parzival/minecraft/velocity/smartmotd/config/model/PingInformation.java
@@ -3,29 +3,27 @@ package space.parzival.minecraft.velocity.smartmotd.config.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.annotation.Nullable;
-
 @Data
 @NoArgsConstructor
 public class PingInformation {
     /**
      * The version string of the server.
      */
-    private @Nullable String version;
+    private String version;
 
     /**
      * The MOTD (Message of the Day) to display to players.
      */
-    private @Nullable String motd;
+    private String motd;
 
     /**
      * The relative path of the data directory to an image file or a base64 encoded image.
      */
-    private @Nullable String favicon;
+    private String favicon;
 
     /**
      * Represents what the players the server purports to have online, its maximum capacity,
      * and a sample of players on the server.
      */
-    private @Nullable PlayerList players;
+    private PlayerList players;
 }
--- src/main/java/space/parzival/minecraft/velocity/smartmotd/config/model/PlayerList.java
+++ src/main/java/space/parzival/minecraft/velocity/smartmotd/config/model/PlayerList.java
@@ -3,7 +3,6 @@ package space.parzival.minecraft.velocity.smartmotd.config.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 @Data
@@ -12,15 +11,15 @@ public class PlayerList {
     /**
      * The number of players currently online.
      */
-    private @Nullable Integer current;
+    private Integer current;
 
     /**
      * The maximum number of players that can be online at the same time.
      */
-    private @Nullable Integer max;
+    private Integer max;
 
     /**
      * A list of player names currently online.
      */
-    private @Nullable List<String> samples;
+    private List<String> samples;
 }
